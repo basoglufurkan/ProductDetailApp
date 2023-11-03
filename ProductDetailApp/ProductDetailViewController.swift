19fbe4e382a3166388be4c306e1cc25677f7bca8
Furkan BAŞOĞLU <furkan.basoglu@karaca.com>
2023-11-03 22:57:33 +0300

    ui complete

---
+++ ProductDetailApp/ProductDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProductDetailViewController.swift
+//  ProductDetailApp
+//
+//  Created by Furkan BAŞOĞLU on 3.11.2023.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ ProductDetailApp/ProductRatingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductRatingTableViewCell.swift
+//  ProductDetailApp
+//
+//  Created by Furkan BAŞOĞLU on 3.11.2023.
+//
+
+import UIKit
+
+class ProductRatingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
